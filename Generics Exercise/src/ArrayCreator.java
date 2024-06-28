9e3aa274b35aa0e7b09abcf39df070fd8c9fdc0a
Pepkin <gospodinov.petko@icloud.com>
2024-06-28 15:27:41 +0300

    GenericArray

---
+++ Generics Exercise/src/ArrayCreator.java
@@ -0,0 +1,25 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ArrayCreator<T> {
+
+    public static <T> T[] create(int length, T item) {
+        T[] array = (T[]) new Object[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = item;
+        }
+
+        return array;
+    }
+
+    public static <T> T[] create(Class<T> clazz, int length, T item) {
+        T[] array = (T[]) Array.newInstance(clazz, length);
+
+        for (int i = 0; i < length; i++) {
+            array[i] = item;
+        }
+
+        return array;
+    }
+}
--- Generics Exercise/src/Main.java
+++ Generics Exercise/src/Main.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Jar<Integer> jar = new Jar<>();
-        jar.add(1);
-        jar.add(4);
-        jar.remove();
-        jar.get();
+
+        Integer[] intArray = ArrayCreator.create(5, 42);
+        for (Integer num : intArray) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        
+        String[] strArray = ArrayCreator.create(String.class, 3, "Hello");
+        for (String str : strArray) {
+            System.out.print(str + " ");
+        }
     }
 }
