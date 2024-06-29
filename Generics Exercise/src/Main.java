6cc1cdbece3fb0ca3ac43651cd5f3ee1a3b84bf6
Pepkin <gospodinov.petko@icloud.com>
2024-06-29 12:30:02 +0300

    Custom list

--- Generics Exercise/src/Main.java
+++ Generics Exercise/src/Main.java
@@ -6,19 +6,52 @@ import static java.lang.StringTemplate.STR;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int commands = Integer.parseInt(sc.nextLine());
-        var comparedValue = sc.nextLine();
-        List list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        CustomList<String> list = new CustomList<>();
 
-        for (int i = 0; i < commands; i++) {
-            var input = sc.nextLine();
+        while (true) {
+            String input = scanner.nextLine();
+            String[] commandParts = input.split(" ");
+            String command = commandParts[0];
 
-            list.add(input);
+            if (command.equals("end")) {
+                break;
+            }
+
+            switch (command) {
+                case "Add":
+                    list.add(commandParts[1]);
+                    break;
+                case "Remove":
+                    int indexToRemove = Integer.parseInt(commandParts[1]);
+                    list.remove(indexToRemove);
+                    break;
+                case "Contains":
+                    System.out.println(list.contains(commandParts[1]));
+                    break;
+                case "Swap":
+                    int index1 = Integer.parseInt(commandParts[1]);
+                    int index2 = Integer.parseInt(commandParts[2]);
+                    list.swap(index1, index2);
+                    break;
+                case "Greater":
+                    System.out.println(list.countGreaterThan(commandParts[1]));
+                    break;
+                case "Max":
+                    System.out.println(list.getMax());
+                    break;
+                case "Min":
+                    System.out.println(list.getMin());
+                    break;
+                case "Print":
+                    list.print();
+                    break;
+                default:
+                    System.out.println("Unknown command");
+                    break;
+            }
         }
 
-        Counter counter = new Counter<>(list, comparedValue);
-
-        counter.toString();
+        scanner.close();
     }
 }
